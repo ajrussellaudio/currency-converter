95363db88a6a4f49c5bf5517856a69d253ed875b
Alan Russell <ajrussellaudio@gmail.com>
2024-08-10 23:38:15 +0100

    Add labels

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import { CurrencySelect } from "./components/CurrencySelect";
+import { AmountInput } from "./components/AmountInput";
+import { Result } from "./components/Result";
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState("GBP");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [amount, setAmount] = useState(1);
+
   return (
     <>
-      <CurrencySelect />
+      <label>
+        Amount
+        <AmountInput value={amount} onChange={setAmount} />
+      </label>
+      <label>
+        From
+        <CurrencySelect value={fromCurrency} onChange={setFromCurrency} />
+      </label>
+      <label>
+        To
+        <CurrencySelect value={toCurrency} onChange={setToCurrency} />
+      </label>
+      <label>
+        Result
+        <Result from={fromCurrency} to={toCurrency} amount={amount} />
+      </label>
     </>
   );
 }
--- src/components/AmountInput/AmountInput.tsx
+++ src/components/AmountInput/AmountInput.tsx
@@ -5,14 +5,11 @@ type AmountInputProps = {
 
 export function AmountInput({ value, onChange }: AmountInputProps) {
   return (
-    <>
-      <label htmlFor="amount">Amount</label>
-      <input
-        type="number"
-        id="amount"
-        value={value}
-        onChange={(e) => onChange(Number(e.currentTarget.value))}
-      />
-    </>
+    <input
+      type="number"
+      id="amount"
+      value={value}
+      onChange={(e) => onChange(Number(e.currentTarget.value))}
+    />
   );
 }
